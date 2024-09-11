09970f57689165bc2009808537d468e2d06c1a6d
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-09-11 10:58:05 +0500

    fixed index view to filter out future questions

--- mypolls/polls/tests.py
+++ mypolls/polls/tests.py
@@ -1,7 +1,9 @@
 import datetime
+from venv import create
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
 
@@ -32,3 +34,28 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTest(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_last_question(self):
+        question = create_question(question_text="Past question", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_question_list"], [question])
+
+    def test_future_question(self):
+        create_question(question_text="Future question", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+
